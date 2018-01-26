8c013e7928515f499d7910f2d9d301f560e070d7
Lioncash <mathew1800@gmail.com>
2018-01-26 08:51:48 -0500

    General: Convert multiple namespace specifiers to nested namespace specifiers where applicable
    Makes namespacing a little less noisy

--- src/backend_x64/emit_x64.cpp
+++ src/backend_x64/emit_x64.cpp
@@ -19,8 +19,7 @@
 // TODO: Have ARM flags in host flags and not have them use up GPR registers unless necessary.
 // TODO: Actually implement that proper instruction selector you've always wanted to sweetheart.
 
-namespace Dynarmic {
-namespace BackendX64 {
+namespace Dynarmic::BackendX64 {
 
 using namespace Xbyak::util;
 
@@ -319,5 +318,4 @@ void EmitX64::InvalidateBasicBlocks(const std::unordered_set<IR::LocationDescrip
     }
 }
 
-} // namespace BackendX64
-} // namespace Dynarmic
+} // namespace Dynarmic::BackendX64
